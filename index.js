e7d5932d76b96c06765be435daa5e6553121d2a1
akashchy23 <akashchypuc2@gmail.com>
2025-12-07 17:45:02 +0600

    cheaked the connection of express and cors

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello! Developers")
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
