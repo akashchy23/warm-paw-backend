edc20f92b33b33249fb1b050860c680d19d7a07e
akashchy23 <akashchypuc2@gmail.com>
2025-12-07 17:46:31 +0600

    added mongodb

--- index.js
+++ index.js
@@ -2,13 +2,43 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const port = 3000;
-
 require('dotenv').config();
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7q4cuiv.mongodb.net/?appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        // await client.connect();
+
+       
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
     res.send("Hello! Developers")
