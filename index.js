e7e92a02cf119b47fe7d5cda27b7f44dd9ab5f46
akashchy23 <akashchypuc2@gmail.com>
2025-12-07 17:44:07 +0600

    added env and cors

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const port = 3000;
+
+require('dotenv').config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
